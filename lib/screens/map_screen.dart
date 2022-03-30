af672983f6e280c04964ce84b56ad53e442c838f
mariapru4 <mariapruss616@gmail.com>
2022-03-30 22:21:34 +0300

    complete google maps

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,8 +1,14 @@
-import 'package:delivery_app/providers/location_provider.dart';
+import 'dart:async';
+import 'dart:ui';
+
+// import 'package:delivery_app/providers/location_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart' as geocoding;
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:location/location.dart';
 import 'package:provider/provider.dart';
 
 class MapScreen extends StatefulWidget {
@@ -13,59 +19,158 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  late LatLng currentLocation;
-  late GoogleMapController _mapController;
+  Completer<GoogleMapController> _controller = Completer();
+  LocationData? _currentPosition;
+  LatLng? _latLng;
+  bool _locating = false;
+  geocoding.Placemark? _placemark;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
-  Widget build(BuildContext context) {
-    final locationData = Provider.of<LocationPovider>(context);
-    setState(() {
-      currentLocation = LatLng(locationData.latitude, locationData.longitude);
-    });
-    void onCreated(GoogleMapController controller) {
-      setState(() {
-        _mapController = controller;
-      });
+  void initState() {
+    _getUserLocation();
+    super.initState();
+  }
+
+  Future<LocationData> _getLocationPermission() async {
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return Future.error('Service not enabled');
+      }
     }
 
-    return Scaffold(
-      body: SafeArea(
-        child: Stack(
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return Future.error('Permission Denied');
+      }
+    }
+
+    _locationData = await location.getLocation();
+    return _locationData;
+  }
+
+  _getUserLocation() async {
+    _currentPosition = await _getLocationPermission();
+
+    _goToCurrentPosition(_latLng =
+        LatLng(_currentPosition!.latitude!, _currentPosition!.longitude!));
+  }
+
+  getUserAddress() async {
+    List<geocoding.Placemark> placemarks = await geocoding
+        .placemarkFromCoordinates(_latLng!.latitude, _latLng!.longitude);
+    setState(() {
+      _placemark = placemarks.first;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: currentLocation,
-                zoom: 14.4746,
-              ),
-              zoomControlsEnabled: false,
-              minMaxZoomPreference: MinMaxZoomPreference(1.5, 20.8),
-              myLocationEnabled: true,
-              myLocationButtonEnabled: true,
-              mapType: MapType.normal,
-              mapToolbarEnabled: true,
-              onCameraMove: (CameraPosition position) {
-                locationData.onCameraMove(position);
-              },
-              onMapCreated: onCreated,
-              onCameraIdle: () {
-                // locationData.getMoveCamera();
-              },
-            ),
-            Center(
-                child: Container(
-                    height: 50,
-                    margin: EdgeInsets.only(bottom: 40),
-                    child: Image.asset('images/marker.png'))),
-            Positioned(
-              bottom: 0.0,
-              child: Container(
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    Text(''),
-                  ],
+            Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.75,
+                  decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: Colors.grey))),
+                  child: Stack(
+                    children: [
+                      GoogleMap(
+                        myLocationEnabled: true,
+                        myLocationButtonEnabled: true,
+                        mapType: MapType.terrain,
+                        initialCameraPosition: _kGooglePlex,
+                        onMapCreated: (GoogleMapController controller) {
+                          _controller.complete(controller);
+                        },
+                        onCameraMove: (CameraPosition position) {
+                          setState(() {
+                            _locating = true;
+                            _latLng = position.target;
+                          });
+                        },
+                        onCameraIdle: () {
+                          setState(() {
+                            _locating = false;
+                          });
+                          getUserAddress();
+                        },
+                      ),
+                      Align(
+                        child: Icon(
+                          Icons.location_on,
+                          size: 40,
+                          color: Colors.amber,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      _placemark != null
+                          ? Column(
+                              children: [
+                                Text(
+                                  _locating
+                                      ? 'Locating...'
+                                      : _placemark!.street!,
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Row(
+                                  children: [
+                                    if (_placemark!.subLocality != null)
+                                      Text('${_placemark!.subLocality!}'),
+                                  ],
+                                ),
+                                Text('${_placemark!.locality!}'),
+                                Text('${_placemark!.country!}')
+                              ],
+                            )
+                          : Container(),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  print(_placemark!.toJson());
+                                },
+                                child: Text('Confirm Location')),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
               ),
             )
           ],
@@ -73,4 +178,73 @@ class _MapScreenState extends State<MapScreen> {
       ),
     );
   }
+
+  Future<void> _goToCurrentPosition(LatLng latLng) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        bearing: 192.8334901395799,
+        target: LatLng(latLng.latitude, latLng.longitude),
+        // tilt: 59.440717697143555,
+        zoom: 17.4746)));
+  }
 }
+// late LatLng currentLocation;
+// late GoogleMapController _mapController;
+// @override
+// Widget build(BuildContext context) {
+//   final locationData = Provider.of<LocationPovider>(context);
+//   setState(() {
+//     currentLocation = LatLng(locationData.latitude, locationData.longitude);
+//   });
+//   void onCreated(GoogleMapController controller) {
+//     setState(() {
+//       _mapController = controller;
+//     });
+//   }
+//
+//   return Scaffold(
+//     body: SafeArea(
+//       child: Stack(
+//         children: [
+//           GoogleMap(
+//             initialCameraPosition: CameraPosition(
+//               target: currentLocation,
+//               zoom: 14.4746,
+//             ),
+//             zoomControlsEnabled: false,
+//             minMaxZoomPreference: MinMaxZoomPreference(1.5, 20.8),
+//             myLocationEnabled: true,
+//             myLocationButtonEnabled: true,
+//             mapType: MapType.normal,
+//             mapToolbarEnabled: true,
+//             onCameraMove: (CameraPosition position) {
+//               locationData.onCameraMove(position);
+//             },
+//             onMapCreated: onCreated,
+//             onCameraIdle: () {
+//               // locationData.getMoveCamera();
+//             },
+//           ),
+//           Center(
+//               child: Container(
+//                   height: 50,
+//                   margin: EdgeInsets.only(bottom: 40),
+//                   child: Image.asset('images/marker.png'))),
+//           Positioned(
+//             bottom: 0.0,
+//             child: Container(
+//               height: 200,
+//               width: MediaQuery.of(context).size.width,
+//               color: Colors.white,
+//               child: Column(
+//                 children: [
+//                   Text(''),
+//                 ],
+//               ),
+//             ),
+//           )
+//         ],
+//       ),
+//     ),
+//   );
+// }
